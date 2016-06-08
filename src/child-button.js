08a5d7a406a85640eeabaf603685e0ca603966f5
jukkatolonen <jukka@tripoint.fi>
2016-06-08 13:02:33 +0200

    child buttons styles added

--- src/child-button.js
+++ src/child-button.js
@@ -1,21 +1,27 @@
-'use strict';
+"use strict";
 
-var React = require('react');
-var classnames = require('classnames');
+var React = require("react");
+var classnames = require("classnames");
 
 var ChildButton = React.createClass({
-  render: function(){
-    var iconClass = classnames('mfb-component__child-icon', this.props.icon);
-    var className = classnames('mfb-component__button--child', this.props.className);
-    return (
-      <li>
-        <a href={this.props.href}
-           data-mfb-label={this.props.label}
-           onClick={this.props.onClick}
-           className={className}>
-          <i className={iconClass}></i>
-        </a>
-      </li>
+  displayName: "ChildButton",
+
+  render: function render() {
+    var iconClass = classnames("mfb-component__child-icon", this.props.icon);
+	var iconClassNotRot = classnames("mfb-component__child-iconNotRotation", this.props.iconNotRotation);
+    var className = classnames("mfb-component__button--child", this.props.className);
+    return React.createElement(
+      "li",
+      null,
+      React.createElement(
+        "a",
+        { href: this.props.href,
+          "data-mfb-label": this.props.label,
+          onClick: this.props.onClick,
+          className: className },
+        React.createElement("i", { className: iconClass }),
+		React.createElement("i", { className: iconClassNotRot })
+      )
     );
   }
 });
--- src/main-button.js
+++ src/main-button.js
@@ -1,35 +1,37 @@
-'use strict';
+"use strict";
 
-var React = require('react');
-var classnames = require('classnames');
+var React = require("react");
+var classnames = require("classnames");
 
 var MainButton = React.createClass({
-  getDefaultProps: function(){
+  displayName: "MainButton",
+
+  getDefaultProps: function getDefaultProps() {
     return {
-      href: '#',
-      onClick: function(){},
-      iconResting: '',
-      iconActive: '',
+      href: "#",
+      onClick: function onClick() {},
+      iconResting: "",
+      iconActive: "",
       label: null
     };
   },
-  render: function(){
-    var iconResting = classnames('mfb-component__main-icon--resting', this.props.iconResting);
-    var iconActive = classnames('mfb-component__main-icon--active', this.props.iconActive);
-    var mainClass = classnames('mfb-component__button--main', this.props.className);
-    if(this.props.label){
-      return (
-        <a href={this.props.href} className={mainClass} onClick={this.props.onClick} data-mfb-label={this.props.label}>
-          <i className={iconResting}></i>
-          <i className={iconActive}></i>
-        </a>
+  render: function render() {
+    var iconResting = classnames("mfb-component__main-icon--resting", this.props.iconResting);
+    var iconActive = classnames("mfb-component__main-icon--active", this.props.iconActive);
+    var mainClass = classnames("mfb-component__button--main", this.props.className);
+    if (this.props.label) {
+      return React.createElement(
+        "a",
+        { href: this.props.href, className: mainClass, onClick: this.props.onClick, "data-mfb-label": this.props.label},
+        React.createElement("i", { className: iconResting }),
+        React.createElement("i", { className: iconActive })
       );
     } else {
-      return (
-        <a href={this.props.href} className={mainClass} onClick={this.props.onClick}>
-          <i className={iconResting}></i>
-          <i className={iconActive}></i>
-        </a>
+      return React.createElement(
+        "a",
+        { href: this.props.href, className: mainClass, onClick: this.props.onClick },
+        React.createElement("i", { className: iconResting }),
+        React.createElement("i", { className: iconActive })
       );
     }
   }
